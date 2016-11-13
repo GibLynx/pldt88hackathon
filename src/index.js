d51de19f6a5b6c06cf7e007ac1feae29a4476f42
Leon Carlo V. Valencia <lynx0123@gmail.com>
2016-11-13 12:18:27 +0800

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,241 @@
+'use strict';
+
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        if (event.session.application.applicationId !== "amzn1.ask.skill.71c22360-6de6-415f-a0f6-57b5f48f4299") {
+            context.fail("Invalid Application ID");
+        }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // handle yes/no intent after the user has been prompted
+    if (session.attributes && session.attributes.userPromptedToContinue) {
+        delete session.attributes.userPromptedToContinue;
+        if ("AMAZON.NoIntent" === intentName) {
+            handleFinishSessionRequest(intent, session, callback);
+        } else if ("AMAZON.YesIntent" === intentName) {
+            handleRepeatRequest(intent, session, callback);
+        }
+    }
+
+    // dispatch custom intents to handlers here
+    if ("BillingIntent" === intentName) {
+        handleBillingRequest(intent, session, callback);
+    } else if ("DontKnowIntent" === intentName) {
+        handleUnsupportedRequest(intent, session, callback);
+    } else if ("AMAZON.YesIntent" === intentName) {
+        handleUnsupportedRequest(intent, session, callback);
+    } else if ("AMAZON.NoIntent" === intentName) {
+        handleUnsupportedRequest(intent, session, callback);
+    } else if ("AMAZON.RepeatIntent" === intentName) {
+        handleRepeatRequest(intent, session, callback);
+    } else if ("AMAZON.HelpIntent" === intentName) {
+        handleGetHelpRequest(intent, session, callback);
+    } else if ("AMAZON.StopIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else if ("AMAZON.CancelIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else {
+        throw "Invalid intent";
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // Add any cleanup logic here
+}
+
+// ------- Skill specific business logic -------
+
+var CARD_TITLE = "PLDTCSR";
+
+function getWelcomeResponse(callback) {
+    var sessionAttributes = {},
+        speechOutput = "Welcome to PLDT customer service. ",
+        shouldEndSession = false,
+        repromptText = "How may I help you today? ",
+        i, j;
+
+    speechOutput += repromptText;
+    sessionAttributes = {
+        "speechOutput": repromptText,
+        "repromptText": repromptText,
+    };
+    callback(sessionAttributes,
+        buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleBillingRequest(intent, session, callback) {
+    // Ensure that session.attributes has been initialized
+    if (!session.attributes) {
+        session.attributes = {};
+    }
+
+    // Set a flag to track that we're in the Help state.
+    session.attributes.userPromptedToContinue = true;
+
+    var speechOutput = "Your bill for the month is 1,000 pesos. ",
+        repromptText = speechOutput,
+        shouldEndSession = false;
+
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession));
+}
+
+function handleUnsupportedRequest(intent, session, callback) {
+    // Ensure that session.attributes has been initialized
+    if (!session.attributes) {
+        session.attributes = {};
+    }
+
+    // Set a flag to track that we're in the Help state.
+    session.attributes.userPromptedToContinue = true;
+
+    var speechOutput = "I'm sorry. I'm not yet equipped to help you with that. ",
+        repromptText = "Would you like to be connected to a live customer service representative? ",
+        shouldEndSession = false;
+
+    speechOutput += repromptText;
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession));
+}
+
+function handleRepeatRequest(intent, session, callback) {
+    if (!session.attributes || !session.attributes.speechOutput) {
+        getWelcomeResponse(callback);
+    } else {
+        callback(session.attributes,
+            buildSpeechletResponseWithoutCard(session.attributes.speechOutput, session.attributes.repromptText, false));
+    }
+}
+
+function handleGetHelpRequest(intent, session, callback) {
+    // Ensure that session.attributes has been initialized
+    if (!session.attributes) {
+        session.attributes = {};
+    }
+
+    // Set a flag to track that we're in the Help state.
+    session.attributes.userPromptedToContinue = true;
+
+    var speechOutput = "As of the moment I can only answer questions with billing. ",
+        repromptText = "What do you need help with? ",
+        shouldEndSession = false;
+
+    speechOutput += repromptText;
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession));
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Good bye!", "", true));
+}
+
+// ------- Helper functions to build responses -------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
